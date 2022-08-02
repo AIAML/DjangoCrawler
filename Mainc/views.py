259bc74c7de43f650176dba7b858354927675a9f
Majid Hemmati <majid.hemmati@yahoo.com>
2022-08-02 12:38:25 +0430

    start

--- Mainc/views.py
+++ Mainc/views.py
@@ -259,12 +259,9 @@ class ReadSitemapView(View):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
         }
-
         xmlDict = {}
-
         r = requests.get(url)
         xml = r.text
-
         soup = BeautifulSoup(xml)
         sitemapTags = soup.find_all("sitemap")
         #sitemapTags = soup.find_all("url")
